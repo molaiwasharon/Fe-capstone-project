16cf4feeaf1abe654231b977c7a63cce4367655e
Lebo Molaiwa <molaiwasharon@gmail.com>
2024-10-02 19:40:52 +0200

    added footer

---
+++ weather-dash/src/Components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        This project was coded by
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          {" "}
+          Sharon Molaiwa{" "}
+        </a>
+        and is
+        <a
+          href="https://github.com/molaiwasharon/Fe-capstone-project.git"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          on GitHub{" "}
+        </a>
+        and
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          {" "}
+          hosted on Netlify
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
